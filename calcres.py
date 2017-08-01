d2e7193e63bc22da7ec1373c5cfd520689995431
reinaldomizutani <30627739+reinaldomizutani@users.noreply.github.com>
2017-08-01 13:08:32 -0300

    added which resistors are associated to each value

--- calcres.py
+++ calcres.py
@@ -1,7 +1,10 @@
 resistores = []
 resarr = []
+resreg = []
 i = 0
-
+#---------------------------------------------------------------------------------
+#-------receive data from file and passes to list 'resistores'
+#---------------------------------------------------------------------------------
 with open('calcres.txt', 'r') as source:
     for line in source:
         resistores.append(line.strip('\n'))
@@ -14,22 +17,32 @@ while i < 24:
 
 i=0
 j=1
-
+numero = 0
+#---------------------------------------------------------------------------------
+#-------calculates equivalent resistance of all possible pairs writes them in another list
+#---------------------------------------------------------------------------------
 
 while i<24:
     while j<24:
         aux = (resistores[i]*resistores[j])/(resistores[i]+resistores[j])
         resarr.append(aux)
+        prim = str(resistores[i])
+        seg = str(resistores[j])
+        resreg.append(prim + ' + ' + seg)
         j+=1
+        numero += 1
     i+=1
     j=i+1
 
+    #list1, list2 = zip(*sorted(zip(list1, list2)))
+resarr, resreg = zip(*sorted(zip(resarr, resreg)))
 
+'''
 for item in resistores:
-    resarr.append(float(item))
+    resarr.append(float(item))'''
 
-resarr.sort()
-
+i=0
 with open('result.txt', 'w') as result:
-    for item in resarr:
-        result.write(str(item) + '\n')
+    while i < numero:
+        result.write(str(resarr[i]) + ' = ' + str(resreg[i]) + '\n')
+        i += 1
